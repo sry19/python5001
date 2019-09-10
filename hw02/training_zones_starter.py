d4644ce479b3d4951d7ad62e49342356101935b5
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-10 16:29:07 -0700

    more clearly

--- hw02/training_zones_starter.py
+++ hw02/training_zones_starter.py
@@ -4,26 +4,32 @@ def main():
 
     print("=======================================")
 
-    #TODO: Fill in the rest of the necessary code here
+    #compute the maximum heart rate and reserve
     max_heart_rate = 208 - 0.7 * age
     reserve=max_heart_rate-restHR
     print("Your heart rate reserve is:",reserve,"bpm")
     print("Here is a breakdown of your training zones:")
+    #compute zone1
     zone1low=restHR+reserve*0.5
     zone1high=restHR+reserve*0.6
     print("Zone 1:",round(zone1low,2),"to",round(zone1high,2),"bpm")
+    #compute zone2
     zone2low=restHR+reserve*0.6+0.01
     zone2high=restHR+reserve*0.7
     print("Zone 2:",round(zone2low,2),"to",round(zone2high,2),"bpm")
+    #compute zone3
     zone3low=restHR+reserve*0.7+0.01
     zone3high=restHR+reserve*0.8
     print("Zone 3:",round(zone3low,2),"to",round(zone3high,2),"bpm")
+    #compute zone4
     zone4low=restHR+reserve*0.8+0.01
     zone4high=restHR+reserve*0.93
     print("Zone 4:",round(zone4low,2),"to",round(zone4high,2),"bpm")
+    #compute zone5
     zone5low=restHR+reserve*0.93+0.01
     zone5high=restHR+reserve
     print("Zone 5:",round(zone5low,2),"to",round(zone5high,2),"bpm")
+
     print("=======================================")
 
 main()
