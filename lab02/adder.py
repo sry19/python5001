0f2522bc63552478a888b34b055f2a9d2d2d0ce3
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-11 10:39:39 -0700

    put adder in main

--- lab02/adder.py
+++ lab02/adder.py
@@ -1,3 +1,4 @@
+#print the sum of two integers
 x=int(input("Enter a first value: "))
 y=int(input("Enter a second value: "))
 print("The sum of",x,"+",y,"is",x+y)
---
+++ lab02/exercise3.py
@@ -0,0 +1,7 @@
+def main():
+    #print the sum of two integers
+    x=int(input("Enter a first value: "))
+    y=int(input("Enter a second value: "))
+    print("The sum of",x,"+",y,"is",x+y)
+
+main()
