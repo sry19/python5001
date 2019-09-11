ac368e36a05ae57926da9222e766ffaa9512df6b
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-11 11:36:51 -0700

    finish lab02

--- lab02/trig_functions.py
+++ lab02/trig_functions.py
@@ -1,9 +1,14 @@
 import math
 
 def main():
+    #input angle
     angle=float(input("Enter an angle: "))
+
+    #compute cosine and sine
     cosine=math.cos(math.radians(angle))
     sine=math.sin(math.radians(angle))
+    
+    #print cosine and sine
     print("The cosine of",angle,"is",cosine)
     print("The sine of",angle,"is",sine)
 
