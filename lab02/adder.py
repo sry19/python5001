8081cd8bb61e0d96cdcf94142ab83c46fb7593a8
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-17 08:28:02 -0700

    change the format lab02

--- lab02/adder.py
+++ lab02/adder.py
@@ -1,4 +1,4 @@
-#print the sum of two integers
-x=int(float(input("Enter a first value: ")))
-y=int(float(input("Enter a second value: ")))
-print("The sum of",x,"+",y,"is",x+y)
+# print the sum of two integers
+x = int(float(input("Enter a first value: ")))
+y = int(float(input("Enter a second value: ")))
+print("The sum of", x, "+", y, "is", x+y)
--- lab02/weather.py
+++ lab02/weather.py
@@ -1,44 +1,44 @@
-#input the highest and lowest tempratures
-highest_temp=[73,77,71,67,62,65,63,65,67,69]
-lowest_temp=[58,60,60,57,53,54,55,55,55,56]
+# input the highest and lowest tempratures
+highest_temp = [73, 77, 71, 67, 62, 65, 63, 65, 67, 69]
+lowest_temp = [58, 60, 60, 57, 53, 54, 55, 55, 55, 56]
 
 #compute the difference between the highest and the lowest temprature values predicted for the 10 day forecast
-def diff_of_temp(highest_temp,lowest_temp):
-    max_high_temp=highest_temp[0]
+def diff_of_temp(highest_temp, lowest_temp):
+    max_high_temp = highest_temp[0]
     for i in highest_temp:
-        if i>max_high_temp:
-            max_high_temp=i
+        if i > max_high_temp:
+            max_high_temp = i
 
-    min_low_temp=lowest_temp[0]
+    min_low_temp = lowest_temp[0]
     for i in lowest_temp:
-        if i <min_low_temp:
-            min_low_temp=i
+        if i < min_low_temp:
+            min_low_temp = i
 
-    difference_high_low=max_high_temp-min_low_temp
+    difference_high_low = max_high_temp - min_low_temp
     print("The difference between the highest and the lowest temprature values predicted for the 10 day forecast:",difference_high_low,"Fahrenheit")
 
-#input tempratures at noon
-noon_temp=[66,69,66,64,60,60,59,60,62,63]
+# input tempratures at noon
+noon_temp = [66, 69, 66, 64, 60, 60, 59, 60, 62, 63]
 
-#compute the average temperature at noons
+# compute the average temperature at noons
 def ave_noon_temp(noon_temp):
-    sum_temp=0
+    sum_temp = 0
     for i in noon_temp:
-        sum_temp+=i
-    ave_temp=sum_temp/10
-    print("The average temperature at noon predicted for the 10 day forecast is:",ave_temp,"Fahrenheit")
+        sum_temp += i
+    ave_temp = sum_temp / 10
+    print("The average temperature at noon predicted for the 10 day forecast is:", ave_temp, "Fahrenheit")
 
-#convert the temperature from Fahrenheit to Celsius
+# convert the temperature from Fahrenheit to Celsius
 def convert_temp(highest_temp):
-    max_high_temp=highest_temp[0]
+    max_high_temp = highest_temp[0]
     for i in highest_temp:
-        if i>max_high_temp:
-            max_high_temp=i
-    c_temp=(max_high_temp-32)/1.8
-    print("The highest temperature predicted for the 10 day forecast is",max_high_temp,"Fahrenheit.",end=" ")
-    print("It is converted to",c_temp,"Celsius")
+        if i > max_high_temp:
+            max_high_temp = i
+    c_temp = (max_high_temp - 32) / 1.8
+    print("The highest temperature predicted for the 10 day forecast is", max_high_temp, "Fahrenheit.", end=" ")
+    print("It is converted to", c_temp, "Celsius")
 
-#call these functions
-diff_of_temp(highest_temp,lowest_temp)
+# call these functions
+diff_of_temp(highest_temp, lowest_temp)
 ave_noon_temp(noon_temp)
 convert_temp(highest_temp)
