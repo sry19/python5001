05abaa5f9586f39f8e6138bac17f06e710969587
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-18 14:56:41 -0700

    finish capitalized_vowels

---
+++ lab03/capitalized_vowels.py
@@ -0,0 +1,21 @@
+
+
+def main():
+
+    def capitalized_vowels():
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        x = input("Please input a string:")
+        new_x = ''
+        for i in x:
+            if i.lower() in vowels:
+                new_x += i.upper()
+            else:
+                new_x += i.lower()
+        print(new_x)
+
+
+    capitalized_vowels()
+
+
+main()
+
