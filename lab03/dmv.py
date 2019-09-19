e308c70da7cc71a82ccff2efa5c00b742cd62267
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-18 17:59:27 -0700

    add capitalized

--- lab03/dmv.py
+++ lab03/dmv.py
@@ -4,11 +4,12 @@ def main():
 
     def dmv():
         print("Welcome to the DMV (estimated wait time is 3 hours)")
-        first, middle, last = input("Please enter your first, middle, and last name:\n").split(sep = ' ')
+        first_bf, middle_bf, last_bf = input("Please enter your first, middle, and last name:\n").split(sep = ' ')
+        first, middle, last = first_bf.capitalize(), middle_bf.capitalize(), last_bf.capitalize()
         month, day, year = input("Enter date of birth (MM/DD/YY):\n").split(sep = '/')
         print("-------------------------------------")
         print("Washington Driver License")
-        print("DL", random.randint(1000000,9999999))
+        print("DL", random.randint(1000000, 9999999))
         print("LN", last)
         print("FN", first, middle)
         print("DOB", month + '/' + day + '/' + year)
