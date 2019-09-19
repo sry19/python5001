ed047c9584f28fe1f05747b0b833b3a78c3b481c
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-18 19:02:31 -0700

    add simple conditions

--- lab03/dmv.py
+++ lab03/dmv.py
@@ -4,9 +4,14 @@ def main():
 
     def dmv():
         print("Welcome to the DMV (estimated wait time is 3 hours)")
-        first_bf, middle_bf, last_bf = input("Please enter your first, middle, and last name:\n").split(sep = ' ')
-        first, middle, last = first_bf.capitalize(), middle_bf.capitalize(), last_bf.capitalize()
+        name = input("Please enter your first, middle, and last name:\n")
+        first = name[:name.find(' ')]
+        last = name[name.rfind(' ')+1:]
+        middle = name[name.find(' ')+1: name.rfind(' ')]
+        first, middle, last = first.capitalize(), middle.capitalize(), last.capitalize()
         month, day, year = input("Enter date of birth (MM/DD/YY):\n").split(sep = '/')
+        while int(month) < 1 or int(month) > 12 or int(day) < 1 or int(day) >31:
+            month, day, year = input("Enter valid date of birth (MM/DD/YY):\n").split(sep = '/')
         print("-------------------------------------")
         print("Washington Driver License")
         print("DL", random.randint(1000000, 9999999))
