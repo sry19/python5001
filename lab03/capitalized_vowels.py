cc7679ac152bfa7ac40a2cb9260c3c1af2a59db5
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-18 19:19:28 -0700

    add comments

--- lab03/capitalized_vowels.py
+++ lab03/capitalized_vowels.py
@@ -6,6 +6,7 @@ def main():
         vowels = ['a', 'e', 'i', 'o', 'u']
         x = input("Please input a string:")
         new_x = ''
+        # lower and upper
         for i in x:
             if i.lower() in vowels:
                 new_x += i.upper()
--- lab03/dmv.py
+++ lab03/dmv.py
@@ -3,16 +3,23 @@ import random
 def main():
 
     def dmv():
+        # print some prompts
         print("Welcome to the DMV (estimated wait time is 3 hours)")
+
+        # input names and captilize 
         name = input("Please enter your first, middle, and last name:\n")
         first = name[:name.find(' ')]
         last = name[name.rfind(' ')+1:]
         middle = name[name.find(' ')+1: name.rfind(' ')]
         first, middle, last = first.capitalize(), middle.capitalize(), last.capitalize()
+
+        # input valid DOB
         month, day, year = input("Enter date of birth (MM/DD/YY):\n").split(sep = '/')
         while int(month) < 1 or int(month) > 12 or int(day) < 1 or int(day) >31:
             month, day, year = input("Enter valid date of birth (MM/DD/YY):\n").split(sep = '/')
         print("-------------------------------------")
+
+        # output
         print("Washington Driver License")
         print("DL", random.randint(1000000, 9999999))
         print("LN", last)
