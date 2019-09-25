db983de5c1ec45f740f853580611736b783e3d31
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-09-24 23:25:18 -0700

    finish triangular.py

---
+++ hw04/triangular.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    # compute the triangular number
+    input_num = int(sys.argv[1])
+    triangular_num = 0
+    for i in range(1, input_num+1):
+        triangular_num += i
+    print(triangular_num)
+
+
+main()
