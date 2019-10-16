a538ea988e18e50e783c4388bb44c500cc1b247d
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-10-15 20:04:12 -0700

    check magic number

--- hw07/luhn_algorithm.py
+++ hw07/luhn_algorithm.py
@@ -1,5 +1,7 @@
 # Author:Ruoyun Sun
 # implement luhn's Algorithm to chexk if an account number is valid
+DIVISOR = 10
+FIR_2_DIGIT = 10
 
 
 def main():
@@ -14,7 +16,7 @@ def main():
     ori_num = acc_num
     acc_num = list(acc_num)
     luhn_algo(acc_num)
-    if isvalid(acc_num):
+    if is_valid(acc_num):
         print("The account number", ori_num, "is valid")
     else:
         print("The account number", ori_num, "is not valid")
@@ -30,20 +32,20 @@ def luhn_algo(acc_num):
         list -> None'''
     for i in range(len(acc_num)-2, -1, -2):
         num = 2 * int(acc_num[i])
-        if num >= 10:
+        if num >= FIR_2_DIGIT:
             str_num = str(num)
             num = int(str_num[0]) + int(str_num[1])
         acc_num[i] = num
 
 
-def isvalid(acc_num):
+def is_valid(acc_num):
     '''If the resulting sum is evenly divisible by 10, the sequence is valid.
      If the resulting sum is not divisible by 10, the sequence is not valued
         list -> Boolean'''
     sums = 0
     for i in range(len(acc_num)):
         sums += int(acc_num[i])
-    return not sums % 10
+    return not sums % DIVISOR
 
 
 main()
