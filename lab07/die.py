f0e030ed462d1d242cad313d7219739cce4425b7
sun.ruoy <sun.ruoy@husky.neu.edu>
2019-10-16 15:34:05 -0700

    snack case

--- lab07/die.py
+++ lab07/die.py
@@ -2,14 +2,14 @@ import random
 
 RANDOM_START = 1
 RANDOM_END = 6
-INIT = 0
+INIT_NUM = 0
 
 
 class Die:
     '''A class representing a Die'''
     def __init__(self):
         '''create a die object, initialize its value as 0'''
-        self.current_value = INIT
+        self.current_value = INIT_NUM
 
     def roll(self):
         '''roll a die
